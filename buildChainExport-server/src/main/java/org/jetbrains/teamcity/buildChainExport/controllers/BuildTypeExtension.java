c22e8c60c48c6273d240431455da85c04391257d
Yegor Yarko <yegor.yarko@jetbrains.com>
2014-09-23 10:25:47 +0200

    More precise isAvailable checks (according to pull request comment)

--- buildChainExport-server/src/main/java/org/jetbrains/teamcity/buildChainExport/controllers/BuildTypeExtension.java
+++ buildChainExport-server/src/main/java/org/jetbrains/teamcity/buildChainExport/controllers/BuildTypeExtension.java
@@ -5,6 +5,7 @@ import jetbrains.buildServer.web.openapi.PagePlaces;
 import jetbrains.buildServer.web.openapi.PlaceId;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.SimplePageExtension;
+import jetbrains.buildServer.web.util.WebUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class BuildTypeExtension extends SimplePageExtension {
@@ -18,6 +19,6 @@ public class BuildTypeExtension extends SimplePageExtension {
 
   @Override
   public boolean isAvailable(@NotNull final HttpServletRequest request) {
-    return request.getRequestURI().contains("/viewType.html");
+    return super.isAvailable(request) && WebUtil.getPathWithoutContext(request).startsWith("/viewType.html");
   }
 }
